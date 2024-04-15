2a2fd4543e4d9699cfbf981de45bff56eb6693fd
jstein <janolestein@gmail.com>
2024-04-15 15:55:01 +0200

    Changed to technical names for Block retrival

--- scripts/content.js
+++ scripts/content.js
@@ -29,149 +29,152 @@ let styles = `
         background-color: #1E94BF;
     }
     
-`
+`;
 
-let styleSheet = document.createElement("style")
-styleSheet.innerText = styles
-document.head.appendChild(styleSheet)
+let styleSheet = document.createElement("style");
+styleSheet.innerText = styles;
+document.head.appendChild(styleSheet);
 
 async function addNav() {
+  await new Promise((resolve) => setTimeout(resolve, 15000));
 
-    await new Promise(resolve => setTimeout(resolve, 15000));
+  const allTechnicalNames = document.querySelectorAll(
+    "[data-test='technicalNamePreview']",
+  );
 
-    const header = document.getElementsByTagName('header')[0];
-    const plusZweierBlock = document.querySelector("[data-flip-id='zJx2FJhEGCpbfhlMMg0x']");
-    const videoBlock = document.querySelector("[data-flip-id='X4z5u8MZ3rhzJ7PPFFQz']");
-    const newsBlock = document.querySelector("[data-flip-id='0nWJjC1t0g0M6REKpm9Z']");
-    const polBlock = document.querySelector("[data-flip-id='KSlflajUrhemq1qjRWM8']");
-    const showBlock = document.querySelector("[data-flip-id='JOmPnca9aI5f4egYxft2']");
-    const sportBlock = document.querySelector("[data-flip-id='Wvv8FMp5BwLZzdlwg0wM']");
-    const plusBlock = document.querySelector("[data-flip-id='MFfOSHTyyA5T2tPNlsOw']");
-    const regioBlock = document.querySelector("[data-flip-id='mUMToRLHaEHWjERvt5Xk']");
-    const geldBlock = document.querySelector("[data-flip-id='G5IRwA3ffOI71Vg0lKjo']");
+  const header = document.getElementsByTagName("header")[0];
+  const plusZweierBlock = Array.from(allTechnicalNames).find(
+    (span) => span.innerText === "MOBILE",
+  );
 
-    
-    
-    const mrec = document.querySelector("[data-flip-id='pi66iXWJFZ3ZKdap5kMb']");
+  const videoCenter = Array.from(allTechnicalNames).find(
+    (span) => span.innerText === "Video-Center",
+  );
+  const newsBlock = Array.from(allTechnicalNames).find(
+    (span) => span.innerText === "news",
+  );
+  const polBlock = Array.from(allTechnicalNames).find(
+    (span) => span.innerText === "politik",
+  );
+  const showBlock = Array.from(allTechnicalNames).find(
+    (span) => span.innerText === "unterhaltung",
+  );
+  const sportBlock = Array.from(allTechnicalNames).find(
+    (span) => span.innerText === "sport",
+  );
+  const plusBlock = Array.from(allTechnicalNames).find(
+    (span) => span.innerText === "bild-plus-artikel",
+  );
+  const regioBlock = Array.from(allTechnicalNames).find(
+    (span) => span.innerText === "regional",
+  );
+  const geldBlock = Array.from(allTechnicalNames).find(
+    (span) => span.innerText === "geld",
+  );
 
-    console.log(header);
-    console.log(newsBlock);
-    
-    const overallDiv = document.createElement("div");
-    overallDiv.setAttribute("id", "overallDiv");
-    header.appendChild(overallDiv);
+  const mrec = document.querySelector("[data-flip-id='pi66iXWJFZ3ZKdap5kMb']");
 
-    const topBtn = document.createElement("button");
-    topBtn.setAttribute("id", "navBtn");
-    topBtn.innerHTML = "Top";
-    overallDiv.appendChild(topBtn);
+  console.log(header);
+  console.log(newsBlock);
 
-    topBtn.addEventListener("click", () => {
-        if(mrec != null){
-            mrec.scrollIntoView(false);
-        }
-        else{
-        const scrollBtn = document.querySelector("[data-test='scrollButton']");
-        scrollBtn.click();
-        }
+  const overallDiv = document.createElement("div");
+  overallDiv.setAttribute("id", "overallDiv");
+  header.appendChild(overallDiv);
 
+  const topBtn = document.createElement("button");
+  topBtn.setAttribute("id", "navBtn");
+  topBtn.innerHTML = "Top";
+  overallDiv.appendChild(topBtn);
 
-    })
+  topBtn.addEventListener("click", () => {
+    if (mrec != null) {
+      mrec.scrollIntoView(false);
+    } else {
+      const scrollBtn = document.querySelector("[data-test='scrollButton']");
+      scrollBtn.click();
+    }
+  });
 
-    const plusZweierBtn = document.createElement("button");
-    plusZweierBtn.setAttribute("id", "navBtn");
-    plusZweierBtn.innerHTML = "Plus Zweier";
-    overallDiv.appendChild(plusZweierBtn);
+  const plusZweierBtn = document.createElement("button");
+  plusZweierBtn.setAttribute("id", "navBtn");
+  plusZweierBtn.innerHTML = "Plus Zweier";
+  overallDiv.appendChild(plusZweierBtn);
 
-    plusZweierBtn.addEventListener("click", () => {
-        plusZweierBlock.scrollIntoView(true);
-    })
+  plusZweierBtn.addEventListener("click", () => {
+    plusZweierBlock.scrollIntoView(true);
+  });
 
-    const videoBtn = document.createElement("button");
-    videoBtn.setAttribute("id", "navBtn");
-    videoBtn.innerHTML = "Video";
-    overallDiv.appendChild(videoBtn);
+  const videoBtn = document.createElement("button");
+  videoBtn.setAttribute("id", "navBtn");
+  videoBtn.innerHTML = "Video";
+  overallDiv.appendChild(videoBtn);
 
-    videoBtn.addEventListener("click", () => {
-        videoBlock.scrollIntoView(false);
-    })
+  videoBtn.addEventListener("click", () => {
+    videoCenter.scrollIntoView(true);
+  });
 
-    const newsBtn = document.createElement("button");
-    newsBtn.setAttribute("id", "navBtn");
-    newsBtn.innerHTML = "News";
-    overallDiv.appendChild(newsBtn);
+  const newsBtn = document.createElement("button");
+  newsBtn.setAttribute("id", "navBtn");
+  newsBtn.innerHTML = "News";
+  overallDiv.appendChild(newsBtn);
 
-    newsBtn.addEventListener("click", () => {
-        newsBlock.scrollIntoView(true);
-    })
+  newsBtn.addEventListener("click", () => {
+    newsBlock.scrollIntoView(true);
+  });
 
+  const polBtn = document.createElement("button");
+  polBtn.setAttribute("id", "navBtn");
+  polBtn.innerHTML = "Politik";
+  overallDiv.appendChild(polBtn);
 
-    const polBtn = document.createElement("button");
-    polBtn.setAttribute("id", "navBtn");
-    polBtn.innerHTML = "Politik";
-    overallDiv.appendChild(polBtn);
+  polBtn.addEventListener("click", () => {
+    polBlock.scrollIntoView(true);
+  });
 
-    polBtn.addEventListener("click", () => {
-        polBlock.scrollIntoView(true);
-    })
+  const showBtn = document.createElement("button");
+  showBtn.setAttribute("id", "navBtn");
+  showBtn.innerHTML = "Show";
+  overallDiv.appendChild(showBtn);
 
-    const showBtn = document.createElement("button");
-    showBtn.setAttribute("id", "navBtn");
-    showBtn.innerHTML = "Show";
-    overallDiv.appendChild(showBtn);
+  showBtn.addEventListener("click", () => {
+    showBlock.scrollIntoView(true);
+  });
 
-    showBtn.addEventListener("click", () => {
-        showBlock.scrollIntoView(true);
-    })
+  const sportBtn = document.createElement("button");
+  sportBtn.setAttribute("id", "navBtn");
+  sportBtn.innerHTML = "Sport";
+  overallDiv.appendChild(sportBtn);
 
-    const sportBtn = document.createElement("button");
-    sportBtn.setAttribute("id", "navBtn");
-    sportBtn.innerHTML = "Sport";
-    overallDiv.appendChild(sportBtn);
+  sportBtn.addEventListener("click", () => {
+    sportBlock.scrollIntoView(true);
+  });
 
-    sportBtn.addEventListener("click", () => {
-        sportBlock.scrollIntoView(true);
-    })
+  const plusBtn = document.createElement("button");
+  plusBtn.setAttribute("id", "navBtn");
+  plusBtn.innerHTML = "Plus";
+  overallDiv.appendChild(plusBtn);
 
+  plusBtn.addEventListener("click", () => {
+    plusBlock.scrollIntoView(true);
+  });
 
-    const plusBtn = document.createElement("button");
-    plusBtn.setAttribute("id", "navBtn");
-    plusBtn.innerHTML = "Plus";
-    overallDiv.appendChild(plusBtn);
+  const regioBtn = document.createElement("button");
+  regioBtn.setAttribute("id", "navBtn");
+  regioBtn.innerHTML = "Regio";
+  overallDiv.appendChild(regioBtn);
 
-    plusBtn.addEventListener("click", () => {
-        plusBlock.scrollIntoView(true);
-    })
+  regioBtn.addEventListener("click", () => {
+    regioBlock.scrollIntoView(true);
+  });
 
-    const regioBtn = document.createElement("button");
-    regioBtn.setAttribute("id", "navBtn");
-    regioBtn.innerHTML = "Regio";
-    overallDiv.appendChild(regioBtn);
+  const geldBtn = document.createElement("button");
+  geldBtn.setAttribute("id", "navBtn");
+  geldBtn.innerHTML = "Wirtschaft";
+  overallDiv.appendChild(geldBtn);
 
-    regioBtn.addEventListener("click", () => {
-        regioBlock.scrollIntoView(true);
-    })
-
-    const geldBtn = document.createElement("button");
-    geldBtn.setAttribute("id", "navBtn");
-    geldBtn.innerHTML = "Wirtschaft";
-    overallDiv.appendChild(geldBtn);
-
-    geldBtn.addEventListener("click", () => {
-        geldBlock.scrollIntoView(true);
-    })
+  geldBtn.addEventListener("click", () => {
+    geldBlock.scrollIntoView(true);
+  });
 }
 
-
 addNav();
-
-
-
-
-
-
-
-
-
-
-
